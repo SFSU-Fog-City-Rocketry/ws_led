8b0c5ca40039bfef52f435be806b6a07285a697e
Odera Nwosu <oderanwosu.j@gmail.com>
2023-06-14 21:59:28 -0700

    added client & removed public dir

--- server/server.js
+++ server/server.js
@@ -8,13 +8,8 @@ const app = express()
 const port = 5000
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.set("view engine", "ejs");
 
 app.get('/', (req, res) => 'hi')
-
-app.use(express.static(__dirname + '/public'))
 
 export const server = app.listen(port, async ()=> {
     console.log( `🚀 ${chalk.blue.bold.italic(`Server is now running at http://localhost:${port}/`)}`)
